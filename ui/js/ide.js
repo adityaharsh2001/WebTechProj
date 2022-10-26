29c2ec9dbea99410bd4843552311f24ee435fbe9
adityaharsh2001 <geekyadi118@gmail.com>
2022-10-26 20:58:41 +0530

    ui done

---
+++ ui/js/ide.js
@@ -0,0 +1,36 @@
+let editor;
+
+window.onload = function() {
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/c_cpp");
+}
+
+function changeLanguage() {
+
+    let language = $("#select-lang").val();
+
+    if(language == 'c' || language == 'c_cpp')editor.session.setMode("ace/mode/c_cpp");
+    else if(language == 'php')editor.session.setMode("ace/mode/php");
+    else if(language == 'python')editor.session.setMode("ace/mode/python");
+    else if(language == 'node')editor.session.setMode("ace/mode/javascript");
+}
+
+function executeCode() {
+
+    $.ajax({
+
+        url: "/app/compiler.php",
+
+        method: "POST",
+
+        data: {
+            language: $("#select-lang").val(),
+            code: editor.getSession().getValue()
+        },
+
+        success: function(response) {
+            $(".output").text(response)
+        }
+    })
+}
